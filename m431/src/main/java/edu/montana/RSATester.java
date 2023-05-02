6148cf3a644ff6ac52db191dc0194107f97a099a
Jacob Hofer <jacobhofer1@gmail.com>
2023-05-02 15:14:33 -0600

    Refactor generation test method

--- m431/src/main/java/edu/montana/RSATester.java
+++ m431/src/main/java/edu/montana/RSATester.java
@@ -3,25 +3,26 @@ package edu.montana;
 public class RSATester {
 
 	public static void main(String[] args) {
-		testGenerationTimes();
+		testGenerationTimes(10, 4096);
 	}
 
-	public static void testGenerationTimes() {
+	public static void testGenerationTimes(int numPerBitSize, int maxBitSize) {
 
-		for (int bitSize = 3; bitSize <= 4096;
-			 bitSize += Math.ceil(bitSize / 10f)) {
+		for (int bitSize = 3; bitSize <= maxBitSize; bitSize++) {
 
 			RSAGenerator gen = new RSAGenerator(bitSize);
 
 			long totalElapsed = 0;
-			for (int i = 0; i < 5; i++) {
+			for (int i = 0; i < numPerBitSize; i++) {
 				long start = System.currentTimeMillis();
 				gen.nextKey();
 				long end = System.currentTimeMillis();
 				totalElapsed += end - start;
 			}
 
-			System.out.println(bitSize + ", " + totalElapsed / 5);
+			System.out.println(
+				bitSize + ", " + totalElapsed / numPerBitSize
+			);
 		}
 	}
 }
