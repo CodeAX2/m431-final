2d8c150aee8628ce99caddf06da12c5a9ee95959
CodeAX2 <jacobhofer1@gmail.com>
2023-05-02 12:58:45 -0600

    Implement RSA breaking

--- m431/src/main/java/edu/montana/App.java
+++ m431/src/main/java/edu/montana/App.java
@@ -7,15 +7,16 @@ public final class App {
 	public static void main(String[] args) {
 
 		// Create a generator for 256-bit messages
-		RSAGenerator generator = new RSAGenerator(256);
+		RSAGenerator generator = new RSAGenerator(16);
 		// Generate the key
 		RSAKey key = generator.nextKey();
 		System.out.println(
 			"Public Key: (N=" + key.getN() + ", E=" + key.getE() + ")"
 		);
 
-		//RSABreaker breaker = new RSABreaker("primes.txt");
-		//breaker.loadPrimes();
+		// Create the breaker
+		RSABreaker breaker = new RSABreaker("primes.txt");
+		breaker.loadPrimes();
 
 		// Ask the user for a number to encrypt
 		Scanner inputScanner = new Scanner(System.in);
@@ -30,5 +31,14 @@ public final class App {
 		// Decrypt the number
 		BigInteger decrypted = key.decrypt(encrypted);
 		System.out.println("Decrypted: " + decrypted);
+
+		// Attempt to break manually
+		BigInteger privD = breaker.breakRSA(key);
+		if (privD != null) {
+			RSAKey customKey =
+				new RSAKey(key.getN(), key.getE(), privD);
+			BigInteger broken = customKey.decrypt(encrypted);
+			System.out.println("Broken: " + broken);
+		}
 	}
 }
--- m431/src/main/java/edu/montana/RSABreaker.java
+++ m431/src/main/java/edu/montana/RSABreaker.java
@@ -17,7 +17,8 @@ public class RSABreaker {
 
 	/**
 	 * Creates a new RSA breaker that uses primes loaded into a file
-	 * @param primesFilePath the path of the file containing the primes
+	 * @param primesFilePath the path of the file containing the
+	 *     primes
 	 */
 	public RSABreaker(String primesFilePath) {
 		this.primesFilePath = primesFilePath;
@@ -45,5 +46,59 @@ public class RSABreaker {
 			return false;
 		}
 		return true;
+	}
+
+	public BigInteger breakRSA(RSAKey key) {
+		BigInteger n = key.getN();
+
+		// Check if n is larger than our max product
+		BigInteger lastPrime = primes.get(primes.size() - 1);
+		if (n.compareTo(lastPrime.multiply(lastPrime)) == 1) {
+			// Too large, not going to find d
+			return null;
+		}
+
+		boolean foundPair = false;
+		BigInteger pairFirst = BigInteger.ONE, pairSecond =
+												   BigInteger.ONE;
+
+		for (int i = 0; i < primes.size() && !foundPair; i++) {
+			pairFirst = primes.get(i);
+
+			for (int j = i; j < primes.size() && !foundPair; j++) {
+				pairSecond = primes.get(j);
+				BigInteger product = pairFirst.multiply(pairSecond);
+
+				if (n.compareTo(product) == -1) {
+					break;
+				} else if (n.compareTo(product) == 0) {
+					foundPair = true;
+				}
+			}
+		}
+
+		// Did not find a pair, return null
+		if (!foundPair)
+			return null;
+
+		// Generate a
+		BigInteger a =
+			pairFirst.subtract(BigInteger.ONE)
+				.multiply(pairSecond.subtract(BigInteger.ONE));
+
+		// Generate d using k
+		BigInteger k = BigInteger.ONE;
+		while (!k.multiply(a)
+					.add(BigInteger.ONE)
+					.mod(key.getE())
+					.equals(BigInteger.ZERO)) {
+			k = k.add(BigInteger.ONE);
+		}
+
+		// Use k to generate d
+		return k.multiply(a)
+			.add(BigInteger.ONE)
+			.divide(key.getE())
+			.mod(a);
 	}
 }
